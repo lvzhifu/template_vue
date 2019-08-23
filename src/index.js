1ce67a09c0e5ee56e9da8117c13d2c4f7bd61c0d
lvzhifu <lvzhifu@beyondsoft.com>
2019-08-23 18:48:59 +0800

    + [init]

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// import 'babel-polyfill'
+// import aa from './onepg.js'
+// import './aa.css'
+import Vue  from "vue"
+let app = new Vue({
+  data() {
+    return {
+      foo: 'adf'
+    }
+  },
+  created() {
+    console.log('开始进入vcvaaadadf了')
+    debugger
+  },
+  // render
+  template: '<a>afdafdadfaf</a>'
+}).$mount('#app')
+// console.log(aa)
+// function ttt() {
+//   aa().then((d) => {
+//     console.log(d)
+//   })
+//   // console.log(aaa)
+//   // console.log('afd')
+// }
+// console.log(ttt())
